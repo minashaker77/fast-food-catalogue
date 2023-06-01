4f8df2efb9e6a88c198324d79ac8b42d70a91eaa
Mina Shaker <minashk1998@gmail.com>
2023-06-01 18:26:31 +0330

    show categories name in nav part

--- src/CategoryList/categoryList.jsx
+++ src/CategoryList/categoryList.jsx
@@ -1,18 +1,37 @@
-import { useEffect } from "react";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { useEffect, useState } from "react";
 import axios from "../axios";
 
-const CategoryList = () =>{
-    useEffect(()=>{
-        const fetchCategories = async()=>{
-            const response = await axios.get('/FoodCategory/categories')
-            console.log(response.data)
-        };
-        fetchCategories();
-    },[]);
-    return (
-        <div>
-            Category List
-        </div>
-    )
-}
+const CategoryList = () => {
+	const [categories, setCategories] = useState([]);
+	useEffect(() => {
+		const fetchCategories = async () => {
+			const response = await axios.get("/FoodCategory/categories");
+			setCategories(response.data);
+		};
+		fetchCategories();
+	}, []);
+	return (
+		<nav className="container mt-n5">
+			<div
+				className="d-flex align-items-center bg-white rounded-3 shadow-lg py-4"
+				style={{ height: "80px" }}>
+				<ul className="nav">
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							همه فست فودها
+						</a>
+					</li>
+					{categories.map((category) => (
+						<li className="nav-item" key={category.id}>
+							<a className="nav-link" href="#">
+								{category.name}
+							</a>
+						</li>
+					))}
+				</ul>
+			</div>
+		</nav>
+	);
+};
 export default CategoryList;
