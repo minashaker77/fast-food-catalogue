0e9a601674834054d19fdf1ced8e5ef7fe700a6b
Mina Shaker <minashk1998@gmail.com>
2023-06-01 17:51:24 +0330

    start project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from "./Header/header";
+
+function App() {
+  return (
+    <div className="wrapper bg-faded-dark">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
