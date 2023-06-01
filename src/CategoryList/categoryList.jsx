f9c1b9772e1b922708d77d0551e5076c3137047d
Mina Shaker <minashk1998@gmail.com>
2023-06-01 18:42:52 +0330

    set loading for category list item

--- src/CategoryList/categoryList.jsx
+++ src/CategoryList/categoryList.jsx
@@ -1,22 +1,26 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { useEffect, useState } from "react";
 import axios from "../axios";
+import Loading from "../Loading/loading";
 
 const CategoryList = () => {
+    const [loading , setLoading] = useState(true);
 	const [categories, setCategories] = useState([]);
 	useEffect(() => {
 		const fetchCategories = async () => {
 			const response = await axios.get("/FoodCategory/categories");
 			setCategories(response.data);
+            setLoading(false);
 		};
 		fetchCategories();
 	}, []);
-	return (
-		<nav className="container mt-n5">
-			<div
-				className="d-flex align-items-center bg-white rounded-3 shadow-lg py-4"
-				style={{ height: "80px" }}>
-				<ul className="nav">
+
+    const renderContent = ()=>{
+        if(loading){
+            return <Loading theme="primary" />
+        }
+        return (
+            <ul className="nav">
 					<li className="nav-item">
 						<a className="nav-link" href="#">
 							همه فست فودها
@@ -30,6 +34,15 @@ const CategoryList = () => {
 						</li>
 					))}
 				</ul>
+        );
+    }
+	return (
+		<nav className="container mt-n5">
+			<div
+				className="d-flex align-items-center bg-white rounded-3 shadow-lg py-4"
+				style={{ height: "80px" }}
+                >
+				{renderContent()}
 			</div>
 		</nav>
 	);
